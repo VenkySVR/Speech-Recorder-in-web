b29ae49e237b7fced91af2e6b5aaab40d06f9442
Venkata Ramana Saniboyina <venky.s.vr13@gmail.com>
2021-09-12 18:10:02 +0530

    Add files via upload

---
+++ asr.js
@@ -0,0 +1,113 @@
+// var mediaRecorder;
+// var recordedChunks;
+var gumStream;
+
+const startButton = document.getElementById('start');
+const stopButton = document.getElementById('stop');
+const audioPlayer = document.getElementById('player');
+const downloadLink = document.getElementById('download');
+stopButton.disabled = true;
+
+// startButton.addEventListener('click', startRecording);
+// stopButton.addEventListener('click', stopRecording);
+
+
+
+// function startRecording() {
+// document.getElementById('status').innerHTML = "Recording...";
+
+// startButton.disabled = true;
+// stopButton.disabled = false;
+
+
+//     navigator.mediaDevices.getUserMedia({
+//         audio: true, video: false
+//     }).then((stream) => {
+//         mediaRecorder = new MediaRecorder(stream);
+//         recordedChunks = [];
+
+//         mediaRecorder.start();
+//         mediaRecorder.addEventListener('dataavailable', (e) => {
+//             recordedChunks.push(e.data);
+//         });
+
+
+//     }).catch((err) => {
+//         startButton.disabled = false;
+//         stopButton.disabled = true;
+//     });
+
+
+// }
+
+// function stopRecording() {
+// document.getElementById('status').innerHTML = "Click on start to record."
+// startButton.disabled = false;
+// stopButton.disabled = true;
+
+//     mediaRecorder.stop();
+
+// const audioBlob = new Blob(recordedChunks);
+// const audioUrl = URL.createObjectURL(audioBlob);
+
+// audioPlayer.controls = true;
+// audioPlayer.src = audioUrl;
+
+//     downloadLink.href = audioUrl;
+// downloadLink.download = new Date().toISOString() + '.wav';
+
+// }
+
+const speechRecorder = (stream) => {
+    const mediaRecorder = new MediaRecorder(stream);
+    const recordedChunks = [];
+    gumStream = stream;
+
+    document.getElementById('status').innerHTML = "Recording...";
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    mediaRecorder.start();
+
+
+    mediaRecorder.addEventListener('dataavailable', (e) => {
+        recordedChunks.push(e.data);
+    });
+
+    mediaRecorder.addEventListener('stop', () => {
+        const audioBlob = new Blob(recordedChunks);
+        const audioUrl = URL.createObjectURL(audioBlob);
+
+        audioPlayer.controls = true;
+        audioPlayer.src = audioUrl;
+        downloadLink.href = audioUrl;
+        downloadLink.download = new Date().toISOString() + '.wav';
+
+    });
+
+
+
+
+    stopButton.addEventListener('click', () => {
+        document.getElementById('status').innerHTML = "Click on start to record."
+        startButton.disabled = false;
+        stopButton.disabled = true;
+        mediaRecorder.stop();
+        gumStream.getAudioTracks()[0].stop();
+        audioPlayer.src = downloadLink.href;
+    });
+
+
+};
+
+
+startButton.addEventListener('click', () => {
+    
+    navigator.mediaDevices.getUserMedia({
+        audio: true, video: false
+    }).then(speechRecorder).catch((err) => {
+        startButton.disabled = false;
+        stopButton.disabled = true;
+    });
+
+});
+
